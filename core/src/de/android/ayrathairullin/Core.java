31370cc9bba409476334a24d6a34319b1de22dcb
Ayrat <ayrat1@mail.ru>
2019-06-11 00:23:05 +0300

    chapter two complete

--- core/src/de/android/ayrathairullin/Core.java
+++ core/src/de/android/ayrathairullin/Core.java
@@ -2,20 +2,27 @@ package de.android.ayrathairullin;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
 
 public class Core extends ApplicationAdapter {
-	public PerspectiveCamera cam;
-	public Model model;
-	public ModelInstance instance;
-	public ModelBatch modelBatch;
-	public Environment environment;
-	
-	@Override
+	private PerspectiveCamera cam;
+	private Model model;
+	private ModelInstance instance;
+	private ModelBatch modelBatch;
+	private Environment environment;
+	private Vector3 position = new Vector3();
+	private float rotation;
+	private static final int MOVEMENT_ACCELERATION = 5;
+    private boolean increment;
+	private float scale = 1;
+
+    @Override
 	public void create () {
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		cam.position.set(10, 10, 10);
@@ -42,10 +49,58 @@ public class Core extends ApplicationAdapter {
 
 		modelBatch.begin(cam);
 		modelBatch.render(instance, environment);
+
+        rotate();
+        movement();
+        scale();
+		updateTransformation();
+
 		modelBatch.end();
 	}
 	
 	@Override
 	public void dispose () {
+	    model.dispose();
 	}
+
+	private void movement() {
+	    instance.transform.getTranslation(position);
+	    if (Gdx.input.isKeyPressed(Input.Keys.W)) position.x += Gdx.graphics.getDeltaTime() * MOVEMENT_ACCELERATION;
+	    if (Gdx.input.isKeyPressed(Input.Keys.D)) position.z += Gdx.graphics.getDeltaTime() * MOVEMENT_ACCELERATION;
+	    if (Gdx.input.isKeyPressed(Input.Keys.A)) position.z -= Gdx.graphics.getDeltaTime() * MOVEMENT_ACCELERATION;
+	    if (Gdx.input.isKeyPressed(Input.Keys.S)) position.x -= Gdx.graphics.getDeltaTime() * MOVEMENT_ACCELERATION;
+
+	    instance.transform.setTranslation(position);
+    }
+
+    private void rotate() {
+//	    if (Gdx.input.isKeyPressed(Input.Keys.NUM_1)) instance.transform.rotate(Vector3.X, Gdx.graphics.getDeltaTime() * 100);
+//	    if (Gdx.input.isKeyPressed(Input.Keys.NUM_2)) instance.transform.rotate(Vector3.Y, Gdx.graphics.getDeltaTime() * 100);
+//	    if (Gdx.input.isKeyPressed(Input.Keys.NUM_3)) instance.transform.rotate(Vector3.Z, Gdx.graphics.getDeltaTime() * 100);
+        rotation = (rotation + Gdx.graphics.getDeltaTime() * 100) % 360;
+        instance.transform.setFromEulerAngles(0, 0, rotation)
+                .trn(position.x, position.y, position.z)
+                .scale(scale, scale, scale);
+    }
+
+    private void updateTransformation() {
+//	    rotation = (rotation + Gdx.graphics.getDeltaTime() * 100) % 360;
+	    instance.transform.setFromEulerAngles(0, 0, rotation)
+        .trn(position.x, position.y, position.z)
+        .scale(scale, scale, scale);
+    }
+
+    private void scale() {
+        if (increment) {
+            scale = (scale + Gdx.graphics.getDeltaTime() / 5);
+            if ( scale >= 1.5f) {
+                increment = false;
+            }else {
+                scale = (scale - Gdx.graphics.getDeltaTime() / 5);
+                if (scale <= .5f) {
+                    increment = true;
+                }
+            }
+        }
+    }
 }
