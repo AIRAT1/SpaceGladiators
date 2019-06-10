267565ca894e27b1d1405bdf371f5c1396e12735
Ayrat <ayrat1@mail.ru>
2019-06-10 23:42:16 +0300

    Initial commit

---
+++ core/src/de/android/ayrathairullin/Core.java
@@ -0,0 +1,62 @@
+package de.android.ayrathairullin;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+
+public class Core extends ApplicationAdapter {
+//	SpriteBatch batch;
+//	Texture img;
+	public PerspectiveCamera cam;
+	public Model model;
+	public ModelInstance instance;
+	public ModelBatch modelBatch;
+	public Environment environment;
+	
+	@Override
+	public void create () {
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10, 10, 10);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		ModelBuilder builder = new ModelBuilder();
+		Material mat = new Material(ColorAttribute.createDiffuse(Color.BLUE));
+		model = builder.createBox(5, 5, 5, mat, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		instance = new ModelInstance(model);
+
+		modelBatch = new ModelBatch();
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, .4f, .4f, .4f, 1f));
+		environment.add(new DirectionalLight().set(.8f, .8f, .8f, -1f, -.8f, -.2f));
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+
+		modelBatch.begin(cam);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
+	}
+	
+	@Override
+	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+	}
+}
